1867938d0362a9994a513e4816542294c5986de6
robbyhecht <robbyhecht@gmail.com>
2019-07-08 12:47:14 -0500

    added components and css

---
+++ src/components/BookDetails.js
@@ -0,0 +1,32 @@
+import React, { useContext } from 'react';
+import { BookContext } from '../contexts/BookContext';
+
+const BookDetails = ({ book }) => {
+  const { dispatch } = useContext(BookContext);
+  return ( 
+    <li onClick={() => dispatch({type: 'REMOVE_BOOK', id: book.id})}>
+      <div className="title">{ book.title }</div>
+      <div className="author">{ book.author }</div>
+    </li>
+  );
+}
+ 
+export default BookDetails;
+
+
+// WITHOUT REDUCER:
+
+// import React, { useContext } from 'react';
+// import { BookContext } from '../contexts/BookContext';
+
+// const BookDetails = ({ book }) => {
+//   const { removeBook } = useContext(BookContext);
+//   return ( 
+//     <li onClick={() => removeBook(book.id)}>
+//       <div className="title">{ book.title }</div>
+//       <div className="author">{ book.author }</div>
+//     </li>
+//   );
+// }
+ 
+// export default BookDetails;
